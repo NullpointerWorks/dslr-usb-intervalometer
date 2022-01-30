02655d83b351a1319d3b4ff5a37d3fb77934c5ec
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2022-01-30 20:40:33 +0100

    update

---
+++ java/src/com/nullpointerworks/intervalometer/Application.java
@@ -0,0 +1,19 @@
+package com.nullpointerworks.intervalometer;
+
+public class Application 
+{
+	public static void main(String[] args) 
+	{
+		new Application();
+	}
+	
+	Application()
+	{
+		
+		
+		
+		
+	}
+	
+	
+}
