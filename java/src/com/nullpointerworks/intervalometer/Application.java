7f94b520b5febea679c82e16925715fb421d42ea
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2022-02-02 20:52:18 +0100

    update

--- java/src/com/nullpointerworks/intervalometer/Application.java
+++ java/src/com/nullpointerworks/intervalometer/Application.java
@@ -1,7 +1,11 @@
 package com.nullpointerworks.intervalometer;
 
+import com.nullpointerworks.intervalometer.view.ApplicationView;
+
 public class Application 
 {
+	public static final String VERSION = "v0.1.0";
+
 	public static void main(String[] args) 
 	{
 		new Application();
@@ -10,9 +14,11 @@ public class Application
 	Application()
 	{
 		
+		ApplicationView vWindow = new ApplicationView();
 		
 		
 		
+		vWindow.setVisible(true);
 	}
 	
 	
---
+++ java/src/com/nullpointerworks/intervalometer/view/ApplicationView.java
@@ -0,0 +1,45 @@
+package com.nullpointerworks.intervalometer.view;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.nullpointerworks.intervalometer.view.awt.AbsoluteLayout;
+
+public class ApplicationView 
+{
+	
+	public JFrame jfWindow;
+	
+	
+	
+	
+	public ApplicationView()
+	{
+		
+		JPanel jpInterface = new JPanel();
+		jpInterface.setLocation(0, 0);
+		jpInterface.setSize(640, 550);
+		jpInterface.setPreferredSize( jpInterface.getSize() );
+		jpInterface.setLayout( new AbsoluteLayout() );
+		jpInterface.setBackground( Color.WHITE );
+		
+		jfWindow = new JFrame();
+		jfWindow.setTitle("DSLR Intervalometer");
+		jfWindow.setResizable(false);
+		jfWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfWindow.setLayout( new AbsoluteLayout() );
+		jfWindow.add(jpInterface);
+		jfWindow.pack();
+		jfWindow.setLocationRelativeTo(null);
+		
+	}
+	
+	public void setVisible(boolean b)
+	{
+		jfWindow.setVisible(b);
+	}
+	
+	
+}
