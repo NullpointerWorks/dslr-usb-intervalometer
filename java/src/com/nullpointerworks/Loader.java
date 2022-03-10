90643aa3461e0dec432422d51b16ead4eb03668e
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2022-03-10 19:54:19 +0100

    Create Loader.java

---
+++ java/src/com/nullpointerworks/Loader.java
@@ -0,0 +1,20 @@
+package com.nullpointerworks;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public final class Loader 
+{
+	public static URL getResource(String path)
+	{
+		return instance.getClass().getResource(path);
+	}
+	
+	public static InputStream getResourceAsStream(String path)
+	{
+		return instance.getClass().getResourceAsStream(path);
+	}
+	
+	private Loader() {}
+	private static Loader instance = new Loader();
+}
